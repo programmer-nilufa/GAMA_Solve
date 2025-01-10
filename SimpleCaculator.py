56855e0a209c888cb9bad45f0e7e721df25493f0
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2025-01-10 08:31:09 +0600

    SimpleCaculator

--- SimpleCaculator.py
+++ SimpleCaculator.py
@@ -1,5 +1,17 @@
-num1 = int(input())
-num2 = int(input())
-oparator = int(input())
-num = num1, oparator, num2
+num1 = int(input("Enter the frist number: "))
+num2 = int(input("Enter the Second number: "))
+oparator = input("Enter the oparetor (+, -, *, /) :")
+
+if oparator == '+':
+    num = num1 + num2
+elif oparator == '-':
+    num = num1 - num2
+elif oparator == '*':
+    num = num1 * num2
+elif oparator == '/':
+    if oparator != 0:
+        num = num1/num2
+    else: print("Numer is divided by zero")
+else: print("Invalid Oparators ")
+
 print(num)
