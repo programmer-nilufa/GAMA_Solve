56de723b31cbde0fbd867846f57e45ab502ce05b
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2025-01-10 09:11:03 +0600

    GadeCalculate

---
+++ GadeCalculate.py
@@ -0,0 +1,14 @@
+number = int(input("Enter the number: "))
+if number > 100:
+    print("Invalid number")
+elif number >= 80:
+    print("A")
+elif number >= 70:
+    print("B")
+elif number >= 60:
+    print("C")
+elif number >= 50:
+    print("D")
+elif number >= 33:
+    print("E")
+else: print("F")
