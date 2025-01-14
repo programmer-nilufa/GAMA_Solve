73354d60a917c8dd62d0a35956331f77b461cdd7
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2025-01-14 23:00:06 +0600

    Circle_OOP

---
+++ Circle_OOP.py
@@ -0,0 +1,10 @@
+class Area:
+    def __init__(self, pi, r, area):
+        self.radius = r
+        self.pai = pi
+        self.area = area
+
+    def circle(self, area):
+        return self.area = self.pi * (self.radius * self)
+
+print(Area.circle())
